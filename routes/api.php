0d3974b1e831c545d0834d03b6dc56bffc208f15
Samuel Terra <samuelterra22@hotmail.com>
2018-02-28 23:01:04 -0300

    Método para retorno de boleto implementado.

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,10 @@
 <?php
 
+use BotMan\BotMan\BotMan;
+use BotMan\BotMan\Messages\Attachments\File;
+use BotMan\BotMan\Messages\Outgoing\OutgoingMessage;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,14 +26,21 @@ Route::post('botman', function (Request $request) {
     $botman = resolve('botman');
 
     // Hello World
-    $botman->hears('foo', function ($bot) {
+    $botman->hears('foo', function (BotMan $bot) {
         // get user info
         $bot->reply(json_encode($bot->getUser()->getInfo()));
         $bot->reply('Hello World');
     });
 
+    // Get boleto
+    $botman->hears('boleto', function (BotMan $bot) {
+        $att = new File('https://www.boletobancario.com/boletofacil/img/boleto-facil-exemplo.pdf');
+        $message = OutgoingMessage::create('')->withAttachment($att);
+        $bot->reply($message);
+    });
+
     // ask
-    $botman->hears('meu nome é {name} e sou de {place}', function ($bot, $name, $place) {
+    $botman->hears('meu nome é {name} e sou de {place}', function (BotMan $bot, $name, $place) {
         $bot->reply("Tudo bem {$name}? Como está o clima em {$place}?");
     });
 
